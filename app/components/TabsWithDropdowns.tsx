ff1d469a3d0c298b106b946238898b1244af7af9
Arnaud Patra <arnaudpatra@hotmail.fr>
2024-10-09 16:46:55 +0200

    qsd

--- app/components/TabsWithDropdowns.tsx
+++ app/components/TabsWithDropdowns.tsx
@@ -11,10 +11,10 @@ interface TabsWithDropdownsProps {
 
 const TabsWithDropdowns: React.FC<TabsWithDropdownsProps> = ({ tabs }) => {
   return (
-    <div className="flex space-x-4">
+    <div className="flex space-x-6">
       {tabs.map((tab, index) => (
         <div key={index} className="relative group">
-          <span className="text-[#666666] cursor-default">{tab.label}</span>
+          <span className="text-main-color cursor-default">{tab.label}</span>
           <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
             <div className="py-1">
               {tab.options.map((option, optionIndex) => (
--- app/page.tsx
+++ app/page.tsx
@@ -7,32 +7,33 @@ import InfoPage from "./components/InfoPage";
 
 export default function Home() {
   const [isInfoOpen, setIsInfoOpen] = useState(false);
+
+  // Constants
   const tabsData = [
     {
-      label: "Series",
-      options: ["Option 1", "Option 2", "Option 3"]
+      label: "Stories",
+      options: ["Story 1", "Story 2", "Story 3"]
     },
     {
       label: "Portfolio",
       options: ["Editorial", "Portraits", "Nature mortes"]
     }
   ];
-  
-  const carouselImages = ["/images/image1.jpg", "/images/image2.png", "/images/image3.jpg", "/images/image4.png"];
+  const carouselImages = ["/carousel/car1.jpg", "/carousel/car2.jpg", "/carousel/car3.jpg", "/carousel/car4.jpg", "/carousel/car5.jpg"];
 
   const toggleInfo = () => setIsInfoOpen(!isInfoOpen);
 
   return (
-    <div className="bg-custom-bg h-screen flex flex-col overflow-hidden">
-      <div className="flex justify-between items-center p-2 sm:p-3">
-
-        <h1 className="text-2xl sm:text-4xl font-sans text-main-color">Disha Patel</h1>
-        
-        <div className="flex items-center">
+    <div className="bg-custom-bg h-screen flex flex-col overflow-hidden font-sans">
+      <div className="flex justify-between items-center p-2 sm:p-5">
+        <h1 className="text-3xl sm:text-5xl font-sans text-main-color pl-4 sm:pl-5">
+          DISHA<br />PATEL
+        </h1>
+        <div className="flex items-center text-xl">
           <TabsWithDropdowns tabs={tabsData} />
           <button
             onClick={toggleInfo}
-            className="ml-2 sm:ml-4 px-2 sm:px-4 py-1 sm:py-2 text-main-color font-medium hover:text text-sm sm:text-base"
+            className="ml-2 sm:ml-4 px-2 sm:px-4 py-1 sm:py-2 text-main-color font-medium hover:text text-xl sm:text-xl"
           >
             Info
           </button>
